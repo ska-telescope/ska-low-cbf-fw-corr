6a37c811bfc63cb24a50b37f18d57f17f8667f88
Giles Babich <giles.babich@csiro.au>
2023-10-27 10:33:13 +0000

    PERENTIE-2171 corr spead updates

--- designs/correlator/src/vhdl/version_pkg.vhd
+++ designs/correlator/src/vhdl/version_pkg.vhd
@@ -4,5 +4,5 @@ USE ieee.std_logic_1164.all;
 PACKAGE version_pkg IS
     constant C_FIRMWARE_MAJOR_VERSION        : std_logic_vector(15 downto 0) := x"0000";
     constant C_FIRMWARE_MINOR_VERSION        : std_logic_vector(15 downto 0) := x"0000";
-    constant C_FIRMWARE_PATCH_VERSION        : std_logic_vector(15 downto 0) := x"0001";                                              
+    constant C_FIRMWARE_PATCH_VERSION        : std_logic_vector(15 downto 0) := x"0002";                                              
 end version_pkg;
