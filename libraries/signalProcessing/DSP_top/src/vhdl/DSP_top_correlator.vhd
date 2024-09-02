f924027cc999623ca30d77aa6941431028eedc69
Giles Babich <giles.babich@csiro.au>
2024-09-02 11:22:58 +1000

    PERENTIE-2609 update vis flag header ID. build

--- libraries/signalProcessing/DSP_top/src/vhdl/DSP_top_correlator.vhd
+++ libraries/signalProcessing/DSP_top/src/vhdl/DSP_top_correlator.vhd
@@ -258,7 +258,7 @@ ARCHITECTURE structure OF DSP_top_correlator IS
 begin
     
     gnd <= (others => '0');
-    o_hbm_reset(4 downto 3) <= x"0";
+    o_hbm_reset(4 downto 3) <= "00";
     --------------------------------------------------------------------------
     -- Signal Processing signal Chains
     --------------------------------------------------------------------------
